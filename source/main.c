4f52f0bcb4179df3c6d82b8115e4effdc8724385
tim suh <65052972+crazynoob458@users.noreply.github.com>
2021-02-28 12:48:35 +1000

    made dumb app but filed

---
+++ source/main.c
@@ -0,0 +1,18 @@
+﻿/*---------------------------------------------------------------------------------
+
+	Basic template code for starting a DS app
+
+---------------------------------------------------------------------------------*/
+#include <nds.h>
+#include <stdio.h>
+//---------------------------------------------------------------------------------
+int main(void) {
+	//---------------------------------------------------------------------------------
+	consoleDemoInit();
+	iprintf("tims stupid button tester \n");
+	iprintf("ugh this was a pain to code \n");
+	iprintf("i f*** up have fun with this empty space");
+	while (1) {
+		swiWaitForVBlank();
+	}
+}
